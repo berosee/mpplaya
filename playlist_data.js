dbb7100579d9960d95684e810a25b97543f2ce0b
cha <rose3470@gmail.com>
2026-03-14 09:37:11 +0900

    202603141

---
+++ playlist_data.js
@@ -0,0 +1,168 @@
+const playlists = {
+    "enka": [
+        {
+            "title": "Four Seasons 'Autumn'-  Allegro (La Caccia)",
+            "url": "music/enka/Four Seasons 'Autumn'-  Allegro (La Caccia).mp3"
+        },
+        {
+            "title": "Four Seasons 'Autumn'- Adagio",
+            "url": "music/enka/Four Seasons 'Autumn'- Adagio.mp3"
+        },
+        {
+            "title": "Four Seasons 'Autumn'- Allegro",
+            "url": "music/enka/Four Seasons 'Autumn'- Allegro.mp3"
+        },
+        {
+            "title": "Four Seasons 'Spring'- Allegro (danza)",
+            "url": "music/enka/Four Seasons 'Spring'- Allegro (danza).mp3"
+        },
+        {
+            "title": "Four Seasons 'Spring'- Allegro",
+            "url": "music/enka/Four Seasons 'Spring'- Allegro.mp3"
+        },
+        {
+            "title": "Four Seasons 'Spring'- Largo e pianissimo",
+            "url": "music/enka/Four Seasons 'Spring'- Largo e pianissimo.mp3"
+        },
+        {
+            "title": "Four Seasons 'Summer'-  Adagio, presto",
+            "url": "music/enka/Four Seasons 'Summer'-  Adagio, presto.mp3"
+        },
+        {
+            "title": "Four Seasons 'Summer'-  Presto (tempo o)",
+            "url": "music/enka/Four Seasons 'Summer'-  Presto (tempo o).mp3"
+        },
+        {
+            "title": "Four Seasons 'Summer'- Allegro non molto",
+            "url": "music/enka/Four Seasons 'Summer'- Allegro non molto.mp3"
+        },
+        {
+            "title": "Four Seasons 'Winter'- Allegro non molto",
+            "url": "music/enka/Four Seasons 'Winter'- Allegro non molto.mp3"
+        },
+        {
+            "title": "Four Seasons 'Winter'- Allegro",
+            "url": "music/enka/Four Seasons 'Winter'- Allegro.mp3"
+        },
+        {
+            "title": "Four Seasons 'Winter'- Largo",
+            "url": "music/enka/Four Seasons 'Winter'- Largo.mp3"
+        }
+    ],
+    "jazz": [
+        {
+            "title": "Andy Williams - Love Is Blue",
+            "url": "music/jazz/Andy Williams - Love Is Blue.flac"
+        },
+        {
+            "title": "Barbra Streisand - Woman In Love",
+            "url": "music/jazz/Barbra Streisand - Woman In Love.flac"
+        },
+        {
+            "title": "Cliff Richard - The Young Ones",
+            "url": "music/jazz/Cliff Richard - The Young Ones.flac"
+        },
+        {
+            "title": "Don Backy - Casabianca",
+            "url": "music/jazz/Don Backy - Casabianca.flac"
+        },
+        {
+            "title": "Irene Cara - Fame",
+            "url": "music/jazz/Irene Cara - Fame.flac"
+        },
+        {
+            "title": "Jewel - Foolish Games",
+            "url": "music/jazz/Jewel - Foolish Games.flac"
+        },
+        {
+            "title": "Leif Garrett - I Was Made for Dancin'",
+            "url": "music/jazz/Leif Garrett - I Was Made for Dancin'.flac"
+        },
+        {
+            "title": "Lynn Anderson - When I Dream",
+            "url": "music/jazz/Lynn Anderson - When I Dream.flac"
+        },
+        {
+            "title": "Patti Page - Tenesee waltz",
+            "url": "music/jazz/Patti Page - Tenesee waltz.flac"
+        },
+        {
+            "title": "Righteous Brothers - Unchained melody",
+            "url": "music/jazz/Righteous Brothers - Unchained melody.flac"
+        },
+        {
+            "title": "Rod Stewart - Sailing",
+            "url": "music/jazz/Rod Stewart - Sailing.flac"
+        },
+        {
+            "title": "Scott McKenzie - San Fransisco",
+            "url": "music/jazz/Scott McKenzie - San Fransisco.flac"
+        },
+        {
+            "title": "The Beatles - Let It Be",
+            "url": "music/jazz/The Beatles - Let It Be.flac"
+        },
+        {
+            "title": "The Beatles - Yesterday",
+            "url": "music/jazz/The Beatles - Yesterday.flac"
+        },
+        {
+            "title": "Village Stompers, The - Washington Square",
+            "url": "music/jazz/Village Stompers, The - Washington Square.flac"
+        }
+    ],
+    "old": [
+        {
+            "title": "Beyonce - TEXAS HOLD EM",
+            "url": "music/old/Beyonce - TEXAS HOLD EM.mp3"
+        },
+        {
+            "title": "Daniel - Blackboard of My Heart",
+            "url": "music/old/Daniel - Blackboard of My Heart.mp3"
+        },
+        {
+            "title": "Daniel - Pretty Little Girl From Omagh",
+            "url": "music/old/Daniel - Pretty Little Girl From Omagh.mp3"
+        },
+        {
+            "title": "Green Green Grass Of Home",
+            "url": "music/old/Green Green Grass Of Home.mp3"
+        },
+        {
+            "title": "Isla Grant - Every Moment Of Every Hour",
+            "url": "music/old/Isla Grant - Every Moment Of Every Hour.mp3"
+        },
+        {
+            "title": "Isla Grant - Only Yesterday",
+            "url": "music/old/Isla Grant - Only Yesterday.mp3"
+        },
+        {
+            "title": "Jonny Cash - Cotton Fields",
+            "url": "music/old/Jonny Cash - Cotton Fields.mp3"
+        },
+        {
+            "title": "Mary Duff - Just Lovin' You",
+            "url": "music/old/Mary Duff - Just Lovin' You.mp3"
+        },
+        {
+            "title": "Mary Duff - Power Of Love",
+            "url": "music/old/Mary Duff - Power Of Love.mp3"
+        },
+        {
+            "title": "Mary Duff - Will You Walk With Me",
+            "url": "music/old/Mary Duff - Will You Walk With Me.mp3"
+        },
+        {
+            "title": "The Carpenters - Yesterday Once More",
+            "url": "music/old/The Carpenters - Yesterday Once More.mp3"
+        },
+        {
+            "title": "Tom Jones - Delilah",
+            "url": "music/old/Tom Jones - Delilah.mp3"
+        },
+        {
+            "title": "dave - The Happy Organ",
+            "url": "music/old/dave - The Happy Organ.flac"
+        }
+    ]
+};
